742cb386614db624d6987dd6818813c828628f43
nbettencourt <nsbettencourt@gmail.com>
2018-02-27 11:43:14 +0000

    Minor updates

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import GoogleMap from '../components/google_map';
+
 
 export default class App extends Component {
     render() {
         return (
-            <div>hellor World</div>
+            <div>hello World</div>
         );
     }
 }

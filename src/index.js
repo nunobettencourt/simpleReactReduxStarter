37fa71361ba8c5c594724c4d3aba7277fd2103f8
nbettencourt <nsbettencourt@gmail.com>
2018-02-27 12:18:00 +0000

    Updated dependencies; Redux Promise replaced by Redux Thunk; Updated test helper

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import promise from 'redux-promise';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 ReactDOM.render(
-<Provider store={createStoreWithMiddleware(reducers, reduxDevTools)}>
-    <App />
-    </Provider>
-    , document.querySelector('#main'));
+    <Provider store={createStoreWithMiddleware(reducers, reduxDevTools)}>
+        <App />
+    </Provider>, document.querySelector('#main')
+);
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,7 +1,7 @@
 import _$ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
+import ReactTestUtils from 'react-dom/test-utils';
 import jsdom from 'jsdom';
 import chai, { expect } from 'chai';
 import chaiJquery from 'chai-jquery';
@@ -9,15 +9,20 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const { JSDOM } = jsdom;
+
+const { document } = (new JSDOM('<!doctype html><html><body></body></html>')).window;
+
+global.document = document;
 global.window = global.document.defaultView;
 global.navigator = global.window.navigator;
 const $ = _$(window);
 
+
 chaiJquery(chai, chai.util, $);
 
 function renderComponent(ComponentClass, props = {}, state = {}) {
-    const componentInstance =  TestUtils.renderIntoDocument(
+    const componentInstance =  ReactTestUtils.renderIntoDocument(
         <Provider store={createStore(reducers, state)}>
             <ComponentClass {...props} />
         </Provider>
